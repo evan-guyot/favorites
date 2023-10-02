6044f7b04e777d373607fa8b490e0f03b97edbbe
Evan GUYOT <evan.guyot2022@campus-eni.fr>
2023-10-02 14:11:32 +0200

    Web App Structure Initialisation

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Favorites</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <p>Here is the index page</p>
+    </main>
+  );
+}

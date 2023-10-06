9d998f13d943f936ac3756cd9cb1ad78c2b9b789
Evan GUYOT <evan.guyot2022@campus-eni.fr>
2023-10-06 16:44:36 +0200

    Adding of Text on the Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,52 @@
 import type { NextPage } from "next";
-import { Typography } from "@mui/material";
+import { Typography, Link, Container, Divider, Chip } from "@mui/material";
 import Layout from "@/components/global/layout";
+import TvIcon from "@mui/icons-material/Tv";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 
 const Home: NextPage = () => {
+  const style = {
+    display: "flex",
+    flexDirection: "column",
+    width: "fit-content",
+    gap: "1em",
+    marginTop: "2em",
+  };
+
   return (
     <Layout title={"Favorites"}>
       <Typography variant="h2" textAlign="center">
         Welcome to <strong>favorites</strong>
       </Typography>
+      <Container sx={style} className="home-paragraphs-container">
+        <Divider>
+          <Chip icon={<MenuBookIcon fontSize="small" />} label="INTRODUCTION" />
+        </Divider>
+        <Typography variant="body1">
+          This web application has been made by{" "}
+          <Link href="https://github.com/evan-guyot">Evan Guyot</Link>
+        </Typography>
+        <Typography variant="body1">
+          Everything's saved on your device, in the local storage. You're the
+          master of your data !
+        </Typography>
+        <Divider>
+          <Chip icon={<TvIcon fontSize="small" />} label="MEDIAS" />
+        </Divider>
+        <Typography variant="body1">
+          Save your favorite Series, Movies, Book with a status and a rate.
+        </Typography>
+        <Divider>
+          <Chip
+            icon={<SettingsApplicationsIcon fontSize="small" />}
+            label="SETTINGS"
+          />
+        </Divider>
+        <Typography variant="body1">
+          Custom YOUR web app, its default color, the font and others soon !
+        </Typography>
+      </Container>
     </Layout>
   );
 };
